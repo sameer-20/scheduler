6578741c8f0d4433bc0f7e799aa7aec42fbf8252
Sameer Nandan Menda <msnandan.20@gmail.com>
2020-09-03 19:55:56 -0400

    Added code to include transition functionality

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,8 +1,24 @@
+import { useState } from "react";
 
-export default function useVisualMode (intial) {
+export default function useVisualMode (initial) {
   
-    const [mode, setMode] = useState(intial);
-    
-    return { mode};
-  
-}
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(mode, replace = false) {
+    if (replace) {
+      history[history.length - 1] = mode;
+    } else {
+      history.push(mode); //To be replaced with an spread operator to avoid side-effect
+    }
+    setMode(mode);
+  }
+
+  function back() {
+    history.pop(); //To be replaced with an spread operator to avoid side-effect
+    setMode(history[history.length - 1]);
+  }
+
+  return { mode, transition, back };
+};
+
