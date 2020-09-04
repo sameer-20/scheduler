b66a7b5071a3bf8b838e34cdaeed2edafa1c7268
Sameer Nandan Menda <msnandan.20@gmail.com>
2020-09-03 20:51:23 -0400

    Added mode constants to the Appointment component

--- src/components/appointment/index.js
+++ src/components/appointment/index.js
@@ -6,11 +6,20 @@ import Show from "components/appointment/Show";
 
 import 'components/appointment/styles.scss'
 
+import useVisualMode from "../../hooks/useVisualMode";
 
 
 export default function Appointment (props) {
-console.log(props)
-const { interviewer, student } = {...props.interview}
+
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  )
+
+  console.log(props)
+  const { interviewer, student } = {...props.interview}
 
   return <article className="appointment">
     <Header time={props.time}/>
