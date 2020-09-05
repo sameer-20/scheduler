ddcc07255a6d28fcc691eddb0c5850c010f8be39
Sameer Nandan Menda <msnandan.20@gmail.com>
2020-09-05 13:47:53 -0400

    Refactored getInterviewersForDay function to return correct data

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -39,26 +39,44 @@ export function getInterview(state, interview) {
 
 // getInterviewersForDay
 
+// export function getInterviewersForDay(state, day) {
+
+//   const appointmentsArray = [];
+//   const interviewersArray = [];
+//   const tempArray = [];
+  
+//   state.days.map(element => {
+//       if (element.name === day)
+//         appointmentsArray.push(...element.appointments);
+//   });
+
+//   appointmentsArray.map(element => {
+//       if (element === state.appointments[element].id && state.appointments[element].interview !== null)
+//         tempArray.push(state.appointments[element].interview.interviewer);
+//   });
+
+//   for (let id of tempArray) {
+//     console.log("Element is", id);
+//     interviewersArray.push(state.interviewers[id]);
+//   }
+  
+//   return interviewersArray;  
+// }
+
+
 export function getInterviewersForDay(state, day) {
 
-  const appointmentsArray = [];
-  const interviewersArray = [];
-  const tempArray = [];
+  const filteredDayObj = state.days.find(element => element.name === day);
   
-  state.days.map(element => {
-      if (element.name === day)
-        appointmentsArray.push(...element.appointments);
-  });
-
-  appointmentsArray.map(element => {
-      if (element === state.appointments[element].id && state.appointments[element].interview !== null)
-        tempArray.push(state.appointments[element].interview.interviewer);
-  });
-
-  for (let id of tempArray) {
-    console.log("Element is", id);
-    interviewersArray.push(state.interviewers[id]);
+  if (!filteredDayObj) {
+    return [];
   }
+
+  const interviewersArray = filteredDayObj.interviewers.map (
+      interviewerId => state.interviewers[interviewerId]
+  );
   
-  return interviewersArray;  
-}
+  return interviewersArray;
+};
+
+
