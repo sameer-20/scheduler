d79104cf6b1afa1aa18d11e8e4a995d12fc7f0b6
Sameer Nandan Menda <msnandan.20@gmail.com>
2020-09-05 17:08:48 -0400

    Addedfunctionalities for Edit and Delete forms

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,6 +4,9 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Form from "components/Appointment/Form";
+import Status from "components/Appointment/Status";
+import Confirm from "components/Appointment/Confirm";
+import Error from "components/Appointment/Error";
 
 import 'components/Appointment/styles.scss'
 
@@ -13,7 +16,9 @@ const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
 const SAVING = "SAVING";
-
+const DELETING = "DELETING";
+const CONFIRM = "CONFIRM";
+const EDIT = "EDIT";
 
 export default function Appointment (props) {
 
@@ -33,6 +38,23 @@ export default function Appointment (props) {
     .then(() => transition(SHOW));
     
   }
+
+  function edit() {
+    transition(EDIT);
+  }
+    
+  function deleting() {
+    transition(CONFIRM);
+  }
+
+
+  function confirm() {
+    transition(DELETING, true);
+  
+    props
+      .cancelInterview(id)
+      .then(() => transition(EMPTY));
+  }
     
   console.log("Displaying props", props)
   
@@ -40,14 +62,35 @@ export default function Appointment (props) {
   <article className="appointment">
     <Header time={time}/>
     {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+    {mode === DELETING && <Status message={"Deleting.."} />}
+    {mode === SAVING && <Status message={"Saving.."} />}
+    {mode === CONFIRM && <Confirm message={"Are you sure to delete?"} onConfirm={confirm} />}
     {mode === SHOW && (
       <Show
         student={interview.student}
         interviewer={interview.interviewer}
+        onEdit={edit}
+        onDelete={deleting}
       />
     )}
 
-    {mode === CREATE && (<Form interviewers={interviewers} onCancel={back} onSave={save} />)}
+    {mode === CREATE && (
+        <Form 
+          interviewers={interviewers} 
+          onCancel={back} 
+          onSave={save}
+        />
+    )}
+
+    {mode === EDIT && (
+        <Form
+          name={interview.student}
+          interviewers={interviewers}
+          interviewer={interview.interviewer.id}
+          onSave={save}
+          onCancel={back}
+        />
+    )}
     
   </article>
   );
