2236551da89a4b6caf5bb7481238618fd473fc3a
Sameer Nandan Menda <msnandan.20@gmail.com>
2020-09-05 19:59:06 -0400

    Refactored code

--- src/components/Application.js
+++ src/components/Application.js
@@ -14,55 +14,47 @@ export default function Application(props) {
 
   const { state, setDay, bookInterview, cancelInterview } = useApplicationData();
 
-  // console.log("Appointments @@", state.appointments);
-  // console.log("Interviewers @@", state.interviewers);
+  const interviewers = getInterviewersForDay(state, state.day);
 
-  const appointments = getAppointmentsForDay(state, state.day);
-  const interviewers = getInterviewersForDay(state, state.day)
+  const appointments = getAppointmentsForDay(state, state.day).map(
+    appointment => {
+      return (
+        <Appointment
+          key={appointment.id}
+          {...appointment}
+          interview={getInterview(state, appointment.interview)}
+          interviewers={interviewers}
+          bookInterview={bookInterview}
+          cancelInterview={cancelInterview}
+        />
+      );
+    }
+  );
   
   return (
     <main className="layout">
       <section className="sidebar">
-      <img
-        className="sidebar--centered"
-        src="images/logo.png"
-        alt="Interview Scheduler"
-      />
-      <hr className="sidebar__separator sidebar--centered" />
-      <nav className="sidebar__menu">
-
-        <DayList
-          days={state.days}
-          day={state.day}
-          setDay={setDay}
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
         />
-
-      </nav>
-      <img
-        className="sidebar__lhl sidebar--centered"
-        src="images/lhl.png"
-        alt="Lighthouse Labs"
-      />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+          <DayList days={state.days} day={state.day} setDay={setDay} />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
       <section className="schedule">
-        {appointments.map(appointment => {
-          const interview = getInterview(state, appointment.interview);        
-          return (
-            <Appointment 
-            key={appointment.id}
-            id={appointment.id}
-            time={appointment.time}
-            interview={interview}
-            interviewers={interviewers}
-            bookInterview={bookInterview}
-            cancelInterview={cancelInterview} 
-            />
-          );
-         })
-        }
+        <section className="schedule">
+          {appointments}
+          <Appointment key="last" time="5pm" />
+        </section>
       </section>
     </main>
   );
 }
-
-
