f7743e062542c2f0b9985497da24c09bd6ec3870
Sameer Nandan Menda <msnandan.20@gmail.com>
2020-09-07 16:55:23 -0400

    Add a sample test to verify mock functions

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -16,5 +16,13 @@ describe("Appointment", () => {
   it("renders without crashing", () => {
     render(<Appointment />);
   });
+
+
+  it("checks the working of a mock function", () => {
+    const fn = jest.fn((a, b) => a + b);
+    fn(1, 2);
+    expect(fn).toHaveReturnedWith(3);
+   });
+   
   
 });
