12fc6718cd2f3e177253390e65fc0c872d949649
Sameer Nandan Menda <msnandan.20@gmail.com>
2020-09-08 15:34:04 -0400

    Updated axios path correctly

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -18,11 +18,34 @@ export default function useApplicationData() {
     Promise.all([
       Promise.resolve(axios.get("/api/days")),
       Promise.resolve(axios.get("/api/appointments")),
-      Promise.resolve(axios.get("api/interviewers")),
+      Promise.resolve(axios.get("/api/interviewers")),
     ]).then((all) => {
       setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
     });
   }, []);
+
+
+  // To update the number of spots remaining
+
+  useEffect(() => {
+    setState(prev => ({...prev, days: prev.days.map(day => ({ ...day, spots: calculateSpots(prev, day.name)}))}));
+  },[state.appointments])
+
+
+  const calculateSpots = function (state, dayName) {
+    const day = state.days.find(day => (day.name === dayName))
+
+    const appObj = day.appointments.map(app =>(state.appointments[app]))
+    
+    let counter = 0
+      for (let app of appObj){
+       if (!app.interview){
+          counter++
+        }
+     }
+    return counter
+  };
+
 
   function bookInterview(id, interview) {
     const appointment = {
