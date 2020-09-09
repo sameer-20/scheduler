06ec2fb0b5c38346f655f15e73f9f06dacde6116
Sameer Nandan Menda <msnandan.20@gmail.com>
2020-09-09 16:49:09 -0400

    Added comments

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,6 +1,16 @@
+// End to End testing using Cypress
+
 describe("Navigation", () => {
   it("should visit root", () => {
     cy.visit("/");
   });
+
+  it("should navigate to Tuesday", () => {
+    cy.visit("/");
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected")
+  });
+
 });
 
