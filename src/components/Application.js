5844640a3b31edf8f62bfff3fe9b1dee6c282140
Sameer Nandan Menda <msnandan.20@gmail.com>
2020-09-11 10:13:00 -0400

    CMinor code refactory and added missing packages

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect }  from "react";
 
 import axios from "axios";
 
-import DayList from "components/DayList"
+import DayList from "components/DayList";
 import Appointment from "components/Appointment";
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from 'helpers/selectors';
 import "components/Application.scss";
